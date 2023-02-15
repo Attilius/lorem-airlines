56fd37f50a21d1b257ef5d494953f7ddd82087c7
Attilius <leo20@freemail.hu>
2023-02-15 22:14:21 +0100

    Added PassengerController.php

---
+++ app/Http/Controllers/PassengerController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Passenger;
+use App\Http\Requests\StorePassengerRequest;
+use App\Http\Requests\UpdatePassengerRequest;
+
+class PassengerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePassengerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePassengerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Passenger  $passenger
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Passenger $passenger)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Passenger  $passenger
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Passenger $passenger)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePassengerRequest  $request
+     * @param  \App\Models\Passenger  $passenger
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePassengerRequest $request, Passenger $passenger)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Passenger  $passenger
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Passenger $passenger)
+    {
+        //
+    }
+}
