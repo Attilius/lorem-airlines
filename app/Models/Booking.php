8386344a8da5824cc09a668a8f913a4d5f2c30a4
Attilius <leo20@freemail.hu>
2023-02-18 22:45:53 +0100

    Modified Booking.php

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Booking extends Model
 {
@@ -20,4 +21,12 @@ class Booking extends Model
         'cabin_class',
         'travel_type',
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
