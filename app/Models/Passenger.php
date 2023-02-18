54f632c1d277910d2a4b52ca0e93428805ccc37c
Attilius <leo20@freemail.hu>
2023-02-18 23:15:20 +0100

    Modified Passenger.php

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Passenger extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'first_name',
+        'last_name',
+        'dateOfBirth',
+        'type'
+    ];
 }
