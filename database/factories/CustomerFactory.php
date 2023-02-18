e4aaf21749a2ec0ac6459ed06fe874dc0ed7de2a
Attilius <leo20@freemail.hu>
2023-02-18 23:05:48 +0100

    Modified CustomerFactory.php

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -17,7 +17,13 @@ class CustomerFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'phone' => $this->faker->phoneNumber(),
+            'dateOfBirth' => $this->faker->dateTime()
         ];
     }
 }
