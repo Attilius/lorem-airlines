6d74166c6a9ea857df3bd31bd5e795ce2c5844fb
Attilius <leo20@freemail.hu>
2023-02-19 23:14:47 +0100

    Modified create_flights_table.php

--- database/migrations/2023_02_17_200721_create_flights_table.php
+++ database/migrations/2023_02_17_200721_create_flights_table.php
@@ -15,6 +15,21 @@ return new class extends Migration
     {
         Schema::create('flights', function (Blueprint $table) {
             $table->id();
+            $table->string('departure_city_name');
+            $table->string('departure_location_code');
+            $table->string('departure_location_name');
+            $table->string('arrival_city_name');
+            $table->string('arrival_location_code');
+            $table->string('arrival_location_name');
+            $table->string('airline_name');
+            $table->string('airline_code');
+            $table->string('flight_number');
+            $table->string('aircraft');
+            $table->date('departure_date');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('flight_duration');
+            $table->string('inflight_services');
             $table->timestamps();
         });
     }
