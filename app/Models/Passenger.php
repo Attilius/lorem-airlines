de590911afe360d2b369e684049f65fef5203455
Attilius <leo20@freemail.hu>
2023-02-20 21:44:32 +0100

    Modified Passenger.php

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Passenger extends Model
 {
@@ -21,4 +22,12 @@ class Passenger extends Model
         'dateOfBirth',
         'type'
     ];
+
+    /**
+     * @return HasMany
+     */
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
