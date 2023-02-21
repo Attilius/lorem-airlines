54c4a32c3e4e34ca9282e0bb40319cf2c6b1a160
Attilius <leo20@freemail.hu>
2023-02-21 22:11:02 +0100

    Modified UpdateCustomerRequest.php

---
+++ app/Http/Requests/V1/UpdateCustomerRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $method = $this->method();
+
+        if($method == 'PUT') {
+            return [
+                'name' => ['required'],
+                'type' => ['required', Rule::in(['I', 'B', 'i', 'b'])],
+                'email' => ['required', 'email'],
+                'address' => ['required'],
+                'city' => ['required'],
+                'state' => ['required'],
+                'postalCode' => ['required'],
+                'phone' => ['required'],
+            ];
+        } else {
+            return [
+                'name' => ['sometimes', 'required'],
+                'type' => ['sometimes', 'required', Rule::in(['I', 'B', 'i', 'b'])],
+                'email' => ['sometimes', 'required', 'email'],
+                'address' => ['sometimes', 'required'],
+                'city' => ['sometimes', 'required'],
+                'state' => ['sometimes', 'required'],
+                'postalCode' => ['sometimes', 'required'],
+                'phone' => ['sometimes', 'required'],
+            ];
+        }
+    }
+
+    /**
+     * @return void
+     */
+    protected function prepareForValidation(): void
+    {
+        if ($this->postalCode) {
+            $this->merge([
+                'postal_code' => $this->postalCode,
+            ]);
+        }
+    }
+}
