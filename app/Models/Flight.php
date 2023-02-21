d62ed93d51e4fbfd0d42ab4efc99a9c3ac7e6440
Attilius <leo20@freemail.hu>
2023-02-21 22:06:10 +0100

    Modified Flight.php

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -4,17 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Flight extends Model
 {
     use HasFactory;
 
     /**
-     * @return HasMany
+     * @return BelongsToMany
      */
-    public function bookings(): HasMany
+    public function bookings(): BelongsToMany
     {
-        return $this->hasMany(Booking::class);
+        return $this->belongsToMany(Booking::class);
     }
 }
