ccc9132627472fd74ec4e4b15c2cd4de5ddd0b6d
Attilius <leo20@freemail.hu>
2023-02-21 22:10:21 +0100

    Modified CustomerController.php

---
+++ app/Http/Controllers/API/V1/CustomerController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Filters\V1\CustomersFilter;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
+use App\Http\Resources\V1\CustomerCollection;
+use App\Http\Resources\V1\CustomerResource;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response|CustomerCollection
+     */
+    public function index(Request $request): Response|CustomerCollection
+    {
+        $filter = new CustomersFilter();
+        $filterItems = $filter->transform($request); //[['column', 'operator', 'value']]
+
+        $includeInvoices = $request->query('includeInvoices');
+        $customers = Customer::where($filterItems);
+
+        if ($includeInvoices) {
+            $customers = $customers->with('invoices');
+        }
+
+        return new CustomerCollection($customers->paginate()->appends($request->query()));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  StoreCustomerRequest  $request
+     * @return Response|CustomerResource
+     */
+    public function store(StoreCustomerRequest $request): Response|CustomerResource
+    {
+        return new CustomerResource(Customer::create($request->all()));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Customer  $customer
+     * @return Response|CustomerResource
+     */
+    public function show(Customer $customer): Response|CustomerResource
+    {
+        $includeInvoices = request()->query('includeInvoices');
+
+        if ($includeInvoices) {
+            return new CustomerResource($customer->loadMissing('invoices'));
+        }
+
+        return new CustomerResource($customer);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\V1\UpdateCustomerRequest  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        $customer->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
