a07114546131b36cd9e77cdb8e5ebaa1844f2b11
Attilius <leo20@freemail.hu>
2023-02-21 22:00:23 +0100

    Modified create_invoices_table.php

--- database/migrations/2023_02_19_061406_create_invoices_table.php
+++ database/migrations/2023_02_19_061406_create_invoices_table.php
@@ -14,14 +14,18 @@ return new class extends Migration
     public function up()
     {
         Schema::create('invoices', function (Blueprint $table) {
-            $table->id();
-            $table->integer('customer_id');
+            $table->increments('id');
+            $table->unsignedInteger('customer_id');
             $table->integer('amount');
             $table->string('currency');
             $table->string('status'); // Billed, Paid, Void
             $table->dateTime('billed_date');
             $table->dateTime('paid_date')->nullable();
             $table->timestamps();
+            $table->foreign('customer_id')
+                ->references('id')
+                ->on('customers')
+                ->onDelete('cascade');
         });
     }
 
