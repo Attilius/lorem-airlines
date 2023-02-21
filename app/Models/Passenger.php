e36a55647f6ebe5f857d268f648c7d1245bab9f9
Attilius <leo20@freemail.hu>
2023-02-21 22:06:34 +0100

    Modified Passenger.php

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Passenger extends Model
 {
@@ -24,10 +24,10 @@ class Passenger extends Model
     ];
 
     /**
-     * @return HasMany
+     * @return BelongsToMany
      */
-    public function bookings(): HasMany
+    public function bookings(): BelongsToMany
     {
-        return $this->hasMany(Booking::class);
+        return $this->belongsToMany(Booking::class);
     }
 }
