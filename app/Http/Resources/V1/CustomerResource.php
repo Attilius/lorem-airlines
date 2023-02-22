a0e53e289e75ad4c2ccda1018f55c1d8d4258163
Attilius <leo20@freemail.hu>
2023-02-22 22:33:00 +0100

    Added CustomerResource.php

---
+++ app/Http/Resources/V1/CustomerResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Support\Arrayable;
+use JsonSerializable;
+
+class CustomerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request): array|Arrayable|JsonSerializable
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'type'=> $this->type,
+            'email' =>$this->email,
+            'address' => $this->address,
+            'city' => $this->city,
+            'state' => $this->state,
+            'postalCode' => $this->postal_code,
+            'phone' => $this->phone,
+            'invoice' => InvoiceResource::collection($this->whenLoaded('invoices')),
+        ];
+    }
+}
