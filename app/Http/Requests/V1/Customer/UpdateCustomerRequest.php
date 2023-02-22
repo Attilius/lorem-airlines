07dfd3f842a9c3da4ee7439b4d7c20d4ab2f08a4
Attilius <leo20@freemail.hu>
2023-02-22 22:29:52 +0100

    Modified UpdateCustomerRequest.php

--- app/Http/Requests/V1/Customer/UpdateCustomerRequest.php
+++ app/Http/Requests/V1/Customer/UpdateCustomerRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\V1;
+namespace App\Http\Requests\V1\Customer;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
@@ -14,7 +14,9 @@ class UpdateCustomerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        $user = $this->user();
+
+        return $user != null && $user->tokenCan('update');
     }
 
     /**
