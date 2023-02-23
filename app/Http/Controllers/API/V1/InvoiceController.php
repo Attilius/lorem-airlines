7325f44d654778b70f0cac6a169755b87c94300d
Attilius <leo20@freemail.hu>
2023-02-23 20:37:53 +0100

    Modified InvoiceController.php

--- app/Http/Controllers/API/V1/InvoiceController.php
+++ app/Http/Controllers/API/V1/InvoiceController.php
@@ -1,21 +1,38 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API\V1;
 
+use App\Filters\V1\InvoicesFilter;
+use App\Http\Controllers\Controller;
+//use App\Http\Requests\StoreInvoiceRequest;
+//use App\Http\Requests\UpdateInvoiceRequest;
+use App\Http\Requests\V1\BulkStoreInvoiceRequest;
 use App\Models\Invoice;
-use App\Http\Requests\StoreInvoiceRequest;
-use App\Http\Requests\UpdateInvoiceRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Resources\V1\InvoiceResource;
+use App\Http\Resources\V1\InvoiceCollection;
+use Illuminate\Support\Arr;
 
 class InvoiceController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response|InvoiceCollection
      */
-    public function index()
+    public function index(Request $request):Response|InvoiceCollection
     {
-        //
+        $filter = new InvoicesFilter();
+        $queryItems = $filter->transform($request); //[['column', 'operator', 'value']]
+
+        if (count($queryItems) == 0) {
+            return new InvoiceCollection(Invoice::paginate());
+        } else {
+            $invoice = Invoice::where($queryItems)->paginate();
+
+            return new InvoiceCollection($invoice->appends($request->query()));
+        }
     }
 
     /**
@@ -39,15 +56,25 @@ class InvoiceController extends Controller
         //
     }
 
+
+    public function bulkstore(BulkStoreInvoiceRequest $request)
+    {
+        $bulk = collect($request->all())->map(function($arr, $key){
+            return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+        });
+
+        Invoice::insert($bulk->toArray());
+    }
+
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Invoice  $invoice
-     * @return \Illuminate\Http\Response
+     * @param  Invoice  $invoice
+     * @return Response|InvoiceResource
      */
-    public function show(Invoice $invoice)
+    public function show(Invoice $invoice): Response|InvoiceResource
     {
-        //
+        return new InvoiceResource($invoice);
     }
 
     /**
