9fb77766d9557385679d37585e1932ec1d6d4bf5
Attilius <leo20@freemail.hu>
2023-02-28 22:15:26 +0100

    Modified AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        config([
+            'auth.defaults.guard' => 'admin',
+            'auth.defaults.passwords' => 'admins'
+        ]);
     }
 }
