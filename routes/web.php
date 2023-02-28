f7c2188cd20d8fb67c87bfddfa01e22562dd14e7
Attilius <leo20@freemail.hu>
2023-02-28 22:20:30 +0100

    Modified web.php

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,24 @@ use App\Http\Controllers\UserProfileController;
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register')
+        'canRegister' => Route::has('register'),
     ]);
+})->name('welcome');
+
+Route::middleware([
+    'auth:web',
+    /*'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',*/
+])->group(function () {
+    Route::get('/home', function () {
+        return Inertia::render('Home');
+    })->name('home');
 });
 
+require 'admin.php';
+
+/*
 Route::get('/setup', function () {
     $credentials = [
         'email' => 'admin@admin.com',
@@ -60,67 +74,4 @@ Route::get('/setup', function () {
         }
     }
     return redirect('/admin/dashboard');
-});
-
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/home', function () {
-        return Inertia::render('Home');
-    })->name('home');
-});
-
-// Route::get('/register', [RegisterController::class, 'create'])->middleware('guest')->name('register');
-// Route::post('/register', [RegisterController::class, 'store'])->middleware('guest')->name('register.perform');
-// Route::get('/admin/login', [LoginController::class, 'show'])->middleware('guest')->name('login');
-// Route::post('/admin/login', [LoginController::class, 'login'])->middleware('guest')->name('login.perform');
-// Route::get('/reset-password', [ResetPassword::class, 'show'])->middleware('guest')->name('reset-password');
-// Route::post('/reset-password', [ResetPassword::class, 'send'])->middleware('guest')->name('reset.perform');
-// Route::get('/change-password', [ChangePassword::class, 'show'])->middleware('guest')->name('change-password');
-// Route::post('/change-password', [ChangePassword::class, 'update'])->middleware('guest')->name('change.perform');
-Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/admin/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
-    Route::get('/admin/rtl', [PageController::class, 'rtl'])->name('rtl');
-    Route::get('/admin/profile', [UserProfileController::class, 'show'])->name('profile');
-    Route::post('/admin/profile', [UserProfileController::class, 'update'])->name('profile.update');
-    Route::get('/admin/profile-static', [PageController::class, 'profile'])->name('profile-static');
-    Route::get('/admin/sign-in-static', [PageController::class, 'signin'])->name('sign-in-static');
-    Route::get('/admin/sign-up-static', [PageController::class, 'signup'])->name('sign-up-static');
-    Route::get('/admin/{page}', [PageController::class, 'index'])->name('page');
-    Route::post('/admin/logout', [LoginController::class, 'logout'])->name('logout');
-});
-/*
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\UserProfileController;
-use App\Http\Controllers\ResetPassword;
-use App\Http\Controllers\ChangePassword;
-
-
-Route::get('/', function () {return redirect('/dashboard');})->middleware('auth');
-	Route::get('/register', [RegisterController::class, 'create'])->middleware('guest')->name('register');
-	Route::post('/register', [RegisterController::class, 'store'])->middleware('guest')->name('register.perform');
-	Route::get('/login', [LoginController::class, 'show'])->middleware('guest')->name('login');
-	Route::post('/login', [LoginController::class, 'login'])->middleware('guest')->name('login.perform');
-	Route::get('/reset-password', [ResetPassword::class, 'show'])->middleware('guest')->name('reset-password');
-	Route::post('/reset-password', [ResetPassword::class, 'send'])->middleware('guest')->name('reset.perform');
-	Route::get('/change-password', [ChangePassword::class, 'show'])->middleware('guest')->name('change-password');
-	Route::post('/change-password', [ChangePassword::class, 'update'])->middleware('guest')->name('change.perform');
-	Route::get('/dashboard', [DashboardController::class, 'index'])->name('home')->middleware('auth');
-Route::group(['middleware' => 'auth'], function () {
-	Route::get('/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
-	Route::get('/rtl', [PageController::class, 'rtl'])->name('rtl');
-	Route::get('/profile', [UserProfileController::class, 'show'])->name('profile');
-	Route::post('/profile', [UserProfileController::class, 'update'])->name('profile.update');
-	Route::get('/profile-static', [PageController::class, 'profile'])->name('profile-static');
-	Route::get('/sign-in-static', [PageController::class, 'signin'])->name('sign-in-static');
-	Route::get('/sign-up-static', [PageController::class, 'signup'])->name('sign-up-static');
-	Route::get('/{page}', [PageController::class, 'index'])->name('page');
-	Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 }); */
