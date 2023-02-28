6f1995ceb75faba864c9bf00bdb27795e1552a21
Attilius <leo20@freemail.hu>
2023-02-28 22:17:26 +0100

    Modified CustomerController.php

--- app/Http/Controllers/API/V1/CustomerController.php
+++ app/Http/Controllers/API/V1/CustomerController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Filters\V1\CustomersFilter;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\Customer\DeleteCustomerRequest;
 use App\Http\Requests\V1\Customer\StoreCustomerRequest;
 use App\Http\Requests\V1\Customer\UpdateCustomerRequest;
-use App\Http\Requests\V1\Customer\DeleteCustomerRequest;
-use App\Http\Resources\V1\CustomerCollection;
-use App\Http\Resources\V1\CustomerResource;
+use App\Http\Resources\V1\Customer\CustomerCollection;
+use App\Http\Resources\V1\Customer\CustomerResource;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
