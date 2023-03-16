5c965146631b293a91a37e45fd512c49b5d9d9c6
Attilius <leo20@freemail.hu>
2023-03-16 21:17:18 +0100

    Modified CrudFilterInterface.php

--- app/Filters/CRUD/CrudFilterInterface.php
+++ app/Filters/CRUD/CrudFilterInterface.php
@@ -4,6 +4,11 @@ namespace App\Filters\CRUD;
 
 interface CrudFilterInterface
 {
+    /**
+     * Create a new filter and add to filter list.
+     *
+     * @return void
+     */
     public function add(): void;
     public function get(): array;
     public function update(): void;
