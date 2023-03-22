40e442c644d6955d43e24439c695633bcfe53e39
Attilius <leo20@freemail.hu>
2023-03-22 19:05:39 +0100

    Modified api.php

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group([
     'prefix' => 'v1',
     'namespace' => 'App\Http\Controllers\API\V1',
-    'middleware' => 'auth:sanctum'
+    //'middleware' => 'auth:sanctum'
 ], function () {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('invoices', InvoiceController::class);
